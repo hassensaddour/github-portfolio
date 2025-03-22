3d4c4c83999808e57186b17bc22b6c6662f940ba
hassensaddour <hassen.saddour@esprit.tn>
2025-03-22 02:12:26 +0100

    navbar editer1

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Portfolio Loaded!");
+
+    const sections = document.querySelectorAll('.section');
+
+    function checkScroll() {
+        sections.forEach(section => {
+            const sectionTop = section.getBoundingClientRect().top;
+            if (sectionTop < window.innerHeight - 100) {
+                section.classList.add('show');
+            }
+        });
+    }
+
+    window.addEventListener('scroll', checkScroll);
+    checkScroll();
 });
